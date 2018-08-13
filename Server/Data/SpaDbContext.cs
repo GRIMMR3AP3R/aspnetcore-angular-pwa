e8329143bc36bb8da91ce1aad1bff6cf0cbf02ae
Robert Grimm <robert.grimm.213@outlook.com>
2018-08-13 07:42:37 -0500

    initial code

---
+++ Server/Data/SpaDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AspnetcoreAngularPwa.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspnetcoreAngularPwa.Data {
+    public class SpaDbContext : DbContext {
+        public SpaDbContext (DbContextOptions<SpaDbContext> options) : base (options) {
+            Database.EnsureCreated ();
+        }
+
+        //List of DB Models - Add your DB models here
+        public DbSet<User> User { get; set; }
+    }
+}
